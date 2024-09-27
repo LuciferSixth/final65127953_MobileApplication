add739b050e1decaf9326b9c5e6034211f7a4052
Phimphot Nualnim <151303038+LuciferSixth@users.noreply.github.com>
2024-09-27 21:33:29 +0700

    Add files via upload

---
+++ before/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart'; // Import sqflite
+import 'web/database_helper.dart'; // Update this to your project structure
+import 'homepage.dart';
+import 'plant_component_page.dart';
+import 'land_use_page.dart';
+import 'addPlant.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Database? database;
+
+  // Initialize the database and handle potential errors
+  try {
+    database = await DatabaseHelper.instance.database;
+  } catch (e) {
+    print('Database initialization error: $e');
+    return; // Exit if the database cannot be initialized
+  }
+
+  runApp(MyApp(database: database!)); // Use non-null assertion
+}
+
+class MyApp extends StatelessWidget {
+  final Database database;
+
+  MyApp({required this.database}); // Use named parameter
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Plant Database App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(database: database), // Pass the database instance here
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/components':
+            return MaterialPageRoute(
+              builder: (context) => PlantComponentPage(database: database),
+            );
+          case '/landUses':
+            return MaterialPageRoute(
+              builder: (context) => LandUsePage(database: database),
+            );
+          case '/addPlant':
+            return MaterialPageRoute(
+              builder: (context) => AddPlantPage(database: database),
+            );
+          default:
+            return MaterialPageRoute(
+              builder: (context) => HomePage(database: database), // Default route
+            );
+        }
+      },
+    );
+  }
+}
